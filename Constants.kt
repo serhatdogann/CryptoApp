b6c5842756d86bee706245aef1678226c5722c41
serhat dogan <73740710+serhatdogann@users.noreply.github.com>
2023-11-21 17:53:21 +0300

    Add files via upload

---
+++ Constants.kt
@@ -0,0 +1,9 @@
+package com.example.kripto.utils
+
+object Constants {
+
+    const val BASE_URL="https://pro-api.coinmarketcap.com/"
+    const val API_KEY="a28a27d1-e78c-4850-908d-e9dd98ffbfef"
+    const val LIMIT="100"
+
+}
